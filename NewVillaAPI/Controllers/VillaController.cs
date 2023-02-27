2dd4e7d582ca14c388f5ab2bfd26709b70739a4c
Divu <d1403kr3@gmail.com>
2023-02-27 11:20:01 +0530

    Learning Web API using c#

---
+++ NewVillaAPI/Controllers/VillaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewVillaAPI.Data;
+using NewVillaAPI.Models;
+
+namespace NewVillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {        
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaStore>> Get()
+        {
+            return Ok(VillaStore.villaList);
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<Villa> Get(int id)
+        {
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            return Ok(villa);
+        }
+        
+        [HttpPost]
+        public ActionResult<Villa> Post([FromBody] Villa value)
+        {
+            VillaStore.villaList.Add(value);
+            return CreatedAtRoute(new { Id= value.Id }, value);
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Villa newVilla)
+        {
+            var villa = VillaStore.villaList.FirstOrDefault(u =>u.Id == id);
+            villa.Name = newVilla.Name;
+           return NoContent();
+            
+        }
+       
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+           var villa = VillaStore.villaList.FirstOrDefault(x=> x.Id == id);
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
+    }
+}
---
+++ NewVillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using NewVillaAPI.Models;
+
+namespace NewVillaAPI.Data
+{
+    public class VillaStore
+    {
+        public static List<Villa> villaList = new List<Villa>()
+        {
+            new Villa{ Id = 1, Name = "Beach view" },
+            new Villa{ Id = 2, Name = "Sea View"}
+        };
+    }
+}
